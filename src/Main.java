ff97870499a5de2286f6e9881e990711784a453d
Vojtěch Brázda <brazdav@spseplzen.cz>
2021-09-22 19:03:31 +0200

    první termín

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main extends Piskvorky{
+    public static void main(String[] args) {
+        Piskvorky piskvorky = new Piskvorky();
+    }
+}
---
+++ src/Piskvorky.java
@@ -0,0 +1,103 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Piskvorky implements ActionListener{
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[225];
+    boolean player1_turn;
+    boolean player2_turn;
+
+    Piskvorky(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Color(43, 135, 255));
+        textfield.setForeground(new Color(255, 255, 255));
+        textfield.setFont(new Font("SansSerif",Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Piškvorky");
+        textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
+
+        button_panel.setLayout(new GridLayout(15,15));
+        button_panel.setBackground(new Color(150,150,150));
+
+        for (int i = 0; i < 225; i++){
+            buttons[i] = new JButton();
+            button_panel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boli",Font.BOLD,50));
+            buttons[i].setBackground(new Color(255, 255, 255));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+
+        }
+
+        title_panel.add(textfield);
+        frame.add(title_panel,BorderLayout.NORTH);
+        frame.add(button_panel);
+
+        firstTurn();
+
+    }
+    public void actionPerformed(ActionEvent e){
+        for(int i=0; i<225; i++){
+            if(e.getSource()==buttons[i]){
+                if (player1_turn){
+                    if (buttons[i].getText()==""){
+                        buttons[i].setForeground(new Color(0,0,0));
+                        buttons[i].setText("X");
+                        player1_turn=false;
+                        textfield.setText("O turn");
+                        check();
+                    }
+
+                    }
+                else {
+                    if (buttons[i].getText()==""){
+                        buttons[i].setForeground(new Color(0, 0, 0));
+                        buttons[i].setText("O");
+                        player1_turn=true;
+                        textfield.setText("X turn");
+                        check();
+                    }
+                }
+            }
+        }
+    }
+    public void firstTurn(){
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        if(random.nextInt(2)==2){
+            player1_turn = true;
+            textfield.setText("X turn");
+        }
+        else{
+            player1_turn = false;
+            textfield.setText("O turn");
+        }
+    }
+    public void check(){
+        int pocet = 0;
+    }
+    public void xWins(int a, int b, int c){
+
+    }
+    public void oWins(int a, int b, int c){
+
+    }
+}
