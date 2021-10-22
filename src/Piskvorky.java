f95420755149ae7c1e31a8696db56e87db77d624
Vojtěch Brázda <brazdav@spseplzen.cz>
2021-10-22 14:37:35 +0200

    vyhodnocování bez ošetření

--- src/Piskvorky.java
+++ src/Piskvorky.java
@@ -12,7 +12,6 @@ public class Piskvorky implements ActionListener{
     JLabel textfield = new JLabel();
     JButton[] buttons = new JButton[225];
     boolean player1_turn;
-    boolean player2_turn;
     int xRada = 0;
     int oRada = 0;
 
@@ -57,7 +56,7 @@ public class Piskvorky implements ActionListener{
         for(int i=0; i<225; i++){
             if(e.getSource()==buttons[i]){
                 if (player1_turn){
-                    if (buttons[i].getText()==""){
+                    if (buttons[i].getText().equals("")){
                         buttons[i].setForeground(new Color(0,0,0));
                         buttons[i].setText("X");
                         player1_turn=false;
@@ -67,7 +66,7 @@ public class Piskvorky implements ActionListener{
 
                 }
                 else {
-                    if (buttons[i].getText()==""){
+                    if (buttons[i].getText().equals("")){
                         buttons[i].setForeground(new Color(0, 0, 0));
                         buttons[i].setText("O");
                         player1_turn=true;
@@ -97,24 +96,143 @@ public class Piskvorky implements ActionListener{
 
 
         checkLeva(tlacitko, znak);
-
-
+        checkPrava(tlacitko, znak);
+        checkHore(tlacitko, znak);
+        checkDole(tlacitko, znak);
+        checkHorePrava(tlacitko, znak);
+        checkHoreLeva(tlacitko, znak);
+        checkDolePrava(tlacitko, znak);
+        checkDoleLeva(tlacitko, znak);
     }
-    public void checkLeva(int tlactiko, String znak){
-        if(tlactiko > 0){
-            tlactiko--;
+    public void checkLeva(int tlacitko, String znak){
+        if(tlacitko != 0 && tlacitko != 15 && tlacitko != 30 && tlacitko != 45 && tlacitko != 60 && tlacitko != 75 && tlacitko != 90 && tlacitko != 105 && tlacitko != 120 && tlacitko != 135 && tlacitko != 150 && tlacitko != 165 && tlacitko != 180 && tlacitko != 195 && tlacitko != 210){
+            tlacitko--;
+        }
+        if (){
+            tlacitko--;
         }
         else return;
-        System.out.println(buttons[tlactiko].getText().toString() + ", " + tlactiko);
-        if (buttons[tlactiko].getText().toString() == znak){
-            if (znak == "X") xWins();
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
             else oWins();
-            checkLeva(tlactiko, znak);
+            checkLeva(tlacitko, znak);
             oRada = 0;
             xRada = 0;
         }
         else return;
+    }
 
+    public void checkPrava(int tlacitko, String znak){
+        if(tlacitko > 0){
+            tlacitko++;
+        }
+        else return;
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
+            else oWins();
+            checkPrava(tlacitko, znak);
+            oRada = 0;
+            xRada = 0;
+        }
+        else return;
+    }
+
+    public void checkHore(int tlacitko, String znak){
+        if(tlacitko > 14){
+            tlacitko -= 15;
+        }
+        else return;
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
+            else oWins();
+            checkHore(tlacitko, znak);
+            oRada = 0;
+            xRada = 0;
+        }
+        else return;
+    }
+
+    public void checkDole(int tlacitko, String znak){
+        if(tlacitko < 210){
+            tlacitko += 15;
+        }
+        else return;
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
+            else oWins();
+            checkDole(tlacitko, znak);
+            oRada = 0;
+            xRada = 0;
+        }
+        else return;
+    }
+
+    public void checkHorePrava(int tlacitko, String znak){
+        if(tlacitko > 0){
+            tlacitko -= 14;
+        }
+        else return;
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
+            else oWins();
+            checkHorePrava(tlacitko, znak);
+            oRada = 0;
+            xRada = 0;
+        }
+        else return;
+    }
+
+    public void checkDolePrava(int tlacitko, String znak){
+        if(tlacitko > 0){
+            tlacitko += 16;
+        }
+        else return;
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
+            else oWins();
+            checkDolePrava(tlacitko, znak);
+            oRada = 0;
+            xRada = 0;
+        }
+        else return;
+    }
+
+    public void checkHoreLeva(int tlacitko, String znak){
+        if(tlacitko > 0){
+            tlacitko -= 16;
+        }
+        else return;
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
+            else oWins();
+            checkHoreLeva(tlacitko, znak);
+            oRada = 0;
+            xRada = 0;
+        }
+        else return;
+    }
+
+    public void checkDoleLeva(int tlacitko, String znak){
+        if(tlacitko > 0){
+            tlacitko += 14;
+        }
+        else return;
+        System.out.println(buttons[tlacitko].getText() + ", " + tlacitko);
+        if (buttons[tlacitko].getText().equals(znak)){
+            if (znak.equals("X")) xWins();
+            else oWins();
+            checkDoleLeva(tlacitko, znak);
+            oRada = 0;
+            xRada = 0;
+        }
+        else return;
     }
     public void xWins(){
         xRada ++;
